78784018b2768978ba4d301ab6435ae7305fa017
Aparajita Fishman <aparajita@aparajita.com>
2015-10-10 12:41:42 -0400

    Updates and enhancements...
    - Using API to generate report.
    - Using local dev dependencies.
    - Allow node >= 0.10.
    - Added handy-dandy postversion script.

--- test/test.js
+++ test/test.js
@@ -3,25 +3,50 @@
 /* eslint-disable no-var, prefer-arrow-callback */
 // jscs: disable requireMultipleVarDecl
 
-var child_process = require("child_process"),
-    fs = require("fs");
+var captureStream = require("capture-stream"),
+    Checker = require("jscs"),
+    fs = require("fs"),
+    path = require("path"),
+    reporter = require("jscs/lib/reporters/inline.js");
 
-var result = child_process.spawnSync(
-        "jscs",
-        "-r inline test/fixtures".split(" "),
-        { encoding: "utf8" }
-    );
+var checker = new Checker();
 
 // jscs: enable
 
-if (result.status === 2)
-{
-    var fixture = fs.readFileSync("test/fixtures/errors.txt", "utf8");
+checker.registerDefaultRules();
+checker.configure(require("../presets/cappuccino.json"));
 
-    if (fixture === result.stdout)
+var errors = [];
+
+fs.readdirSync("test/fixtures").forEach(function(file)
+{
+    if (path.extname(file) === ".js")
+    {
+        var filePath = path.join("test/fixtures", file),
+            source = fs.readFileSync(filePath, { encoding: "utf8" });
+
+        errors.push(checker.checkString(source));
+    }
+});
+
+var restore = captureStream(process.stdout),
+    output;
+
+reporter(errors);
+output = restore(true);
+
+if (process.argv[2] === "generate")
+{
+    fs.writeFileSync("test/fixtures/errors.txt", output);
+    console.log("Fixtures generated");
+}
+else
+{
+    var fixture = fs.readFileSync("test/fixtures/errors.txt", { encoding: "utf8" });
+
+    if (fixture === output)
     {
         console.log("Test passed");
-        process.exit(0);
     }
     else
     {
@@ -29,7 +54,3 @@ if (result.status === 2)
         process.exit(1);
     }
 }
-else if (result.status === 0)
-    console.log("jscs returned no errors!");
-else
-    console.log("jscs returned status %d: %s", result.status, result.stderr);
